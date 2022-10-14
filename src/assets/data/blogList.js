29106989386f66e63016b7b5e394f572e69e47b5
GarryProhor <igorprohorchenko@gmail.com>
2022-10-14 15:02:02 +0300

    news&blogs section

---
+++ src/assets/data/blogList.js
@@ -0,0 +1,47 @@
+import image1 from '../images/blog/01.jpg';
+import image2 from '../images/blog/02.jpg';
+import image3 from '../images/blog/03.jpg';
+import image4 from '../images/blog/04.jpg';
+import image5 from '../images/blog/05.jpg';
+export const blogList = [
+    {
+        id: 1,
+        image: image1,
+        title: 'Виды газонов',
+        text: '',
+        author: 'Ирина Грушник',
+        date: '27 апреля 2022',
+    },
+    {
+        id: 2,
+        image: image2,
+        title: 'Лесной стиль в ландшафтном дизайне',
+        text: '',
+        author: 'Ирина Грушник',
+        date: '6 июня 2022',
+    },
+    {
+        id: 3,
+        image: image3,
+        title: 'Ландшафтный дизайн на склоне',
+        text: '',
+        author: 'Ирина Грушник',
+        date: '13 июля 2022',
+    },
+    {
+        id: 4,
+        image: image4,
+        title: 'Голландский стиль в ландшафтном дизайне',
+        text: '',
+        author: 'Ирина Грушник',
+        date: '21 августа 2022',
+    },
+    {
+        id: 5,
+        image: image5,
+        title: 'Японский стиль и сад в ландшафтном дизайне',
+        text: '',
+        author: 'Ирина Грушник',
+        date: '30 сентября 2022',
+    },
+];
